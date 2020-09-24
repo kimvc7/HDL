635d01c0ba4149a369715b5ff9408feca3a7df1d
Kimberly Villalobos Carballo <kimvc@FVFZT22FL416.tld>
2020-09-23 22:49:55 -0400

    Add validation for selecting number of iterations.

--- StabilityDual/train.py
+++ StabilityDual/train.py
@@ -16,19 +16,22 @@ tf.disable_v2_behavior()
 import numpy as np
 import data
 from NN_model import Model
+import csv
+import itertools
+
 
 import argparse
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("--batch-size", type=int, default=64,
-                            help="batch size")
+parser.add_argument("--batch_range", type=int, nargs='+', default=[32, 64],
+                            help="batch range")
+
+parser.add_argument("--ratio_range", type=int, nargs='+', default=[0.7, 0.8],
+                            help="ratio range")
 
 parser.add_argument("--stable", type=int, default=1,
                             help="number of subsets")
-
-parser.add_argument("--subset-ratio", type=float, default=0.8,
-                            help="how much subset ratio for training")
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -44,10 +47,10 @@ num_output_steps = config['num_output_steps']
 num_summary_steps = config['num_summary_steps']
 num_checkpoint_steps = config['num_checkpoint_steps']
 training_size = config['training_size']
-batch_size = args.batch_size
+#batch_size = args.batch_size
+batch_range = args.batch_range
+ratio_range = args.ratio_range
 stable = args.stable
-subset_ratio = args.subset_ratio
-subset_size = int(training_size*subset_ratio)
 initial_learning_rate = config['initial_learning_rate']
 eta = config['constant_learning_rate']
 learning_rate = tf.train.exponential_decay(initial_learning_rate, 0, 5, 0.85, staircase=True)
@@ -55,103 +58,127 @@ learning_rate = tf.train.exponential_decay(initial_learning_rate, 0, 5, 0.85, st
 # Setting up the data and the model
 mnist = data.load_mnist_data_set(validation_size= (60000 - training_size))
 global_step = tf.Variable(1, name="global_step")
-model = Model(subset_ratio)
-test_dict = {model.x_input: mnist.test._images,
-                model.y_input: mnist.test._labels}
-
-# Setting up the optimizer
-if stable:
-    #CONSTANC STEP SIZE
-    optimizer = tf.train.AdamOptimizer(eta).minimize(model.max_xent, global_step=global_step)
-    #DECREASING STEP SIZE
-    #optimizer = tf.train.AdamOptimizer(learning_rate).minimize(model.xent, global_step=global_step)
-
-else:
-    var_list = [model.W1, model.b1, model.W2, model.b2, model.W3, model.b3]
-    #CONSTANC STEP SIZE
-    optimizer = tf.train.AdamOptimizer(eta).minimize(model.xent, global_step=global_step, var_list=var_list)
-    #DECREASING STEP SIZE
-    #optimizer = tf.train.AdamOptimizer(learning_rate).minimize(model.xent, global_step=global_step, var_list=var_list)
-
-avg_test_acc = 0
-test_accs = {}
-thetas = {}
-num_experiments = config['num_experiments']
-
-for experiment in range(num_experiments):
-  print("Experiment", experiment)
-
-  # Setting up the Tensorboard and checkpoint outputs
-  model_dir = config['model_dir'] + str(datetime.now())
-  if not os.path.exists(model_dir):
-    os.makedirs(model_dir)
-
-  with tf.Session() as sess:
-    # Initialize the summary writer, global variables, and our time counter.
-    summary_writer = tf.summary.FileWriter(model_dir + "/Xent")
-    summary_writer1 = tf.summary.FileWriter(model_dir+ "/Max_Xent")
-    summary_writer2 = tf.summary.FileWriter(model_dir+ "/Accuracy")
-    summary_writer3 = tf.summary.FileWriter(model_dir+ "/Test_Accuracy")
-    sess.run(tf.global_variables_initializer())
-    training_time = 0.0
-
-    # Main training loop
-    for ii in range(max_num_training_steps):
-      x_batch, y_batch = mnist.train.next_batch(batch_size)
-
-      nat_dict = {model.x_input: x_batch,
-                  model.y_input: y_batch}
-
-      # Output
-      if ii % num_output_steps == 0:
-        nat_acc = sess.run(model.accuracy, feed_dict=nat_dict)
-        test_acc = sess.run(model.accuracy, feed_dict=test_dict)
-        nat_xent = sess.run(model.xent, feed_dict=nat_dict)
-        max_xent = sess.run(model.max_xent, feed_dict=nat_dict)
-        print('Step {}:    ({})'.format(ii, datetime.now()))
-        print('    training nat accuracy {:.4}'.format(nat_acc * 100))
-        print('    testing nat accuracy {:.4}'.format(test_acc * 100))
-        print('    Nat Xent {:.4}'.format(nat_xent))
-        print('    Max Xent {:.4}'.format(max_xent))
-
-        #Tensorboard Summaries
-        summary = tf.Summary(value=[
-            tf.Summary.Value(tag='Xent', simple_value= nat_xent),])
-        summary1 = tf.Summary(value=[
-            tf.Summary.Value(tag='Xent', simple_value= max_xent),])
-        summary2 = tf.Summary(value=[
-            tf.Summary.Value(tag='Accuracy', simple_value= nat_acc*100)])
-        summary3 = tf.Summary(value=[
-            tf.Summary.Value(tag='Accuracy', simple_value= test_acc*100)])
-        summary_writer.add_summary(summary, global_step.eval(sess))
-        summary_writer1.add_summary(summary1, global_step.eval(sess))
-        summary_writer2.add_summary(summary2, global_step.eval(sess))
-        summary_writer3.add_summary(summary3, global_step.eval(sess))
-
-        if ii != 0:
-          print('    {} examples per second'.format(
-              num_output_steps * batch_size / training_time))
-          training_time = 0.0
-      
-
-      # Actual training step
-      start = timer()
-      sess.run(optimizer, feed_dict=nat_dict)
-      end = timer()
-      training_time += end - start
 
 
-    #Output test results 
-    test_acc, theta= sess.run((model.accuracy, model.theta), feed_dict=test_dict)
-    test_accs[experiment] = test_acc  * 100
-    thetas[experiment] = theta
-    avg_test_acc += test_acc
+for batch_size, subset_ratio in itertools.product(batch_range, ratio_range): #Parameters chosen with validation
+  print(batch_size, subset_ratio)
+  model = Model(subset_ratio)
+  val_dict = {model.x_input: mnist.validation._images,
+                  model.y_input: mnist.validation._labels}
+  test_dict = {model.x_input: mnist.test._images,
+                  model.y_input: mnist.test._labels}
 
-avg_test_acc  = avg_test_acc/num_experiments
-print('  average testing accuracy {:.4}'.format(avg_test_acc  * 100))
-print('  Theta values', thetas)
-print('  individual accuracies: \n', test_accs)
-print('  Standard deviation {:.2}'.format(np.array([float(test_accs[k]) for k in test_accs]).std()))
+  # Setting up the optimizer
+  if stable:
+      #CONSTANC STEP SIZE
+      optimizer = tf.train.AdamOptimizer(eta).minimize(model.max_xent, global_step=global_step)
+      #DECREASING STEP SIZE
+      #optimizer = tf.train.AdamOptimizer(learning_rate).minimize(model.xent, global_step=global_step)
+
+  else:
+      var_list = [model.W1, model.b1, model.W2, model.b2, model.W3, model.b3]
+      #CONSTANC STEP SIZE
+      optimizer = tf.train.AdamOptimizer(eta).minimize(model.xent, global_step=global_step, var_list=var_list)
+      #DECREASING STEP SIZE
+      #optimizer = tf.train.AdamOptimizer(learning_rate).minimize(model.xent, global_step=global_step, var_list=var_list)
+
+  avg_test_acc = 0
+  test_accs = {}
+  thetas = {}
+  iterations = {}
+  num_experiments = config['num_experiments']
+
+  for experiment in range(num_experiments):
+    print("Experiment", experiment)
+
+    # Setting up the Tensorboard and checkpoint outputs
+    model_dir = config['model_dir'] + str(datetime.now())
+    if not os.path.exists(model_dir):
+      os.makedirs(model_dir)
+
+    with tf.Session() as sess:
+      # Initialize the summary writer, global variables, and our time counter.
+      #summary_writer = tf.summary.FileWriter(model_dir + "/Xent")
+      #summary_writer1 = tf.summary.FileWriter(model_dir+ "/Max_Xent")
+      #summary_writer2 = tf.summary.FileWriter(model_dir+ "/Accuracy")
+      #summary_writer3 = tf.summary.FileWriter(model_dir+ "/Test_Accuracy")
+      sess.run(tf.global_variables_initializer())
+      training_time = 0.0
+
+      # Main training loop
+      best_val_acc = 0
+      test_acc = 0
+      num_iters = 0
+      for ii in range(max_num_training_steps):
+        x_batch, y_batch = mnist.train.next_batch(batch_size)
+
+        nat_dict = {model.x_input: x_batch,
+                    model.y_input: y_batch}
+
+        # Output
+        if ii % num_output_steps == 0:
+          nat_acc = sess.run(model.accuracy, feed_dict=nat_dict)
+          val_acc = sess.run(model.accuracy, feed_dict=val_dict)
+          nat_xent = sess.run(model.xent, feed_dict=nat_dict)
+          max_xent = sess.run(model.max_xent, feed_dict=nat_dict)
+          print('Step {}:    ({})'.format(ii, datetime.now()))
+          print('    training nat accuracy {:.4}'.format(nat_acc * 100))
+          print('    validation nat accuracy {:.4}'.format(val_acc * 100))
+          print('    Nat Xent {:.4}'.format(nat_xent))
+          print('    Max Xent {:.4}'.format(max_xent))
+
+          #Validation
+          if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            num_iters = ii
+            test_acc = sess.run(model.accuracy, feed_dict=test_dict)
+
+          #Tensorboard Summaries
+          #summary = tf.Summary(value=[
+          #    tf.Summary.Value(tag='Xent', simple_value= nat_xent),])
+          #summary1 = tf.Summary(value=[
+          #    tf.Summary.Value(tag='Xent', simple_value= max_xent),])
+          #summary2 = tf.Summary(value=[
+          #    tf.Summary.Value(tag='Accuracy', simple_value= nat_acc*100)])
+          #summary3 = tf.Summary(value=[
+          #    tf.Summary.Value(tag='Accuracy', simple_value= test_acc*100)])
+          #summary_writer.add_summary(summary, global_step.eval(sess))
+          #summary_writer1.add_summary(summary1, global_step.eval(sess))
+          #summary_writer2.add_summary(summary2, global_step.eval(sess))
+          #summary_writer3.add_summary(summary3, global_step.eval(sess))
+
+          if ii != 0:
+            print('    {} examples per second'.format(
+                num_output_steps * batch_size / training_time))
+            training_time = 0.0
+        
+
+        # Actual training step
+        start = timer()
+        sess.run(optimizer, feed_dict=nat_dict)
+        end = timer()
+        training_time += end - start
+
+
+      #Output test results 
+      theta= sess.run(model.theta, feed_dict=test_dict)
+      test_accs[experiment] = test_acc  * 100
+      thetas[experiment] = theta
+      iterations[experiment] = num_iters
+      avg_test_acc += test_acc
+
+  avg_test_acc  = avg_test_acc/num_experiments
+  print('  average testing accuracy {:.4}'.format(avg_test_acc  * 100))
+  print('  Theta values', thetas)
+  print('  individual accuracies: \n', test_accs)
+  std = np.array([float(test_accs[k]) for k in test_accs]).std()
+  print('  Standard deviation {:.2}'.format(np.array([float(test_accs[k]) for k in test_accs]).std()))
+
+  file = open('results.csv', 'a+', newline ='') 
+  with file:
+    writer = csv.writer(file) 
+    writer.writerow([stable, num_experiments, training_size, batch_size, subset_ratio, avg_test_acc, test_accs, std, thetas, max_num_training_steps, iterations])
+
 
 
 
