cc04e4015c04496018ab320c3c165a420c4f3d88
apaskov <68131354+apaskov@users.noreply.github.com>
2021-05-24 12:53:35 -0400

    Update runUCIsc3.sh

--- HDLParallel/StabilityDual/runUCIsc3.sh
+++ HDLParallel/StabilityDual/runUCIsc3.sh
@@ -9,19 +9,11 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:volta:1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=1-20:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mem-per-cpu=4G
 
 #SBATCH --array=0-71
 
 source /etc/profile ; module load anaconda/2020a
 
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 16 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 17 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 18 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 19 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 20 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 21 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 22 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 23 --data_set uci1
-python -u trainUCI.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 24 --data_set uci1
+python -u trainCIFAR.py -m ff --gnum ${SLURM_ARRAY_TASK_ID} --mnum 2 --data_set cifar
