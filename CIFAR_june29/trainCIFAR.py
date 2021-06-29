9b7bf2eb72dd4a93628ed13dc0b4db9765b1c5a9
apaskov <68131354+apaskov@users.noreply.github.com>
2021-06-29 14:18:11 -0400

    Add files via upload

---
+++ CIFAR_june29/trainCIFAR.py
@@ -0,0 +1,303 @@
+"""Trains a model, saving checkpoints and tensorboard summaries along
+   the way."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import json
+import os
+#import shutil
+from timeit import default_timer as timer
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+import input_data
+import itertools
+import utils_model
+import utils_print
+
+import argparse
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--runnum", type=int, default=1, help='which gen param of 40 [0,39]')
+
+parser.add_argument("--batch_range", type=int, nargs='+', default=[64],
+                            help="batch range")
+
+parser.add_argument("--ratio_range", type=float, nargs='+', default=[0.8],
+                            help="ratio range")
+
+parser.add_argument("--model", "-m", type=str, required=True, choices=["ff", "cnn"],
+                            help="model type, either ff or cnn")
+
+parser.add_argument("--stable", action="store_true",
+                            help="stable version")
+
+parser.add_argument("--dropout", type=float, default=1,
+                            help="dropout rate, 1 is no dropout, 0 is all set to 0")
+
+parser.add_argument("--robust", "-r", type=float, default=0,
+                            help="Uncertainty set parameter for training robustness.")
+
+parser.add_argument("--robust_test", "-rtest", type=float,  nargs='+', default=[1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
+                            help="Uncertainty set parameter for evaluating robustness.")
+
+parser.add_argument("--l2", type=float, default=0,
+                            help="l2 regularization rate")
+
+parser.add_argument("--l0", type=float, default=0,
+                            help="l0 regularization rate")
+
+parser.add_argument("--reg_stability", type=float, default=0,
+                            help="reg stability regularization rate")
+
+parser.add_argument("--num_subsets", type=int, default=1,
+                            help="number of subsets for Monte Carlo")
+
+parser.add_argument("--l1_size", type=int, default=256,
+                            help="number of nodes in the first layer, 784 -> l1_size")
+
+parser.add_argument("--l2_size", type=int, default=128,
+                            help="number of nodes in the first layer, l1_size -> l2_size")
+
+parser.add_argument("--cnn_size", type=int, default=32,
+                            help="number of filters in the cnn layers for cnn")
+
+parser.add_argument("--fc_size", type=int, default=128,
+                            help="number of nodes in the dense layer for cnn")
+
+parser.add_argument("--data_set", type=str, default="mnist",
+                            help="number of subsets")
+
+parser.add_argument("--MC", action="store_true",
+                            help="Monte Carlo version")
+
+parser.add_argument("--train_size", type=float, default=0.80,
+                            help="training percent of the data")
+
+parser.add_argument("--lr", type=float, default=0.001,
+                            help="Adam lr")
+
+parser.add_argument("--val_size", type=float, default=0.20,
+                            help="validation percent of the data e.g., 0.25 means 0.25*training size")
+args = parser.parse_args()
+with open('config.json') as config_file:
+    config = json.load(config_file)
+ 
+ 
+possibleparams = [[0.001, 0.001, 128, 0.0001, 1024, 256, 0.85, 1e-07, 1],
+ [0.001, 0.001, 128, 0.0001, 1024, 256, 0.85, 1e-08, 1],
+ [0.001, 0.001, 128, 0.0001, 1024, 256, 1, 1e-07, 1],
+ [0.001, 0.001, 128, 0.0001, 1024, 256, 1, 1e-08, 1],
+ [0.01, 1e-05, 64, 0.0001, 1024, 256, 0.85, 1e-07, 1],
+ [0.01, 1e-05, 64, 0.0001, 1024, 256, 0.85, 1e-08, 1],
+ [0.01, 1e-05, 64, 0.0001, 1024, 256, 1, 1e-07, 1],
+ [0.01, 1e-05, 64, 0.0001, 1024, 256, 1, 1e-08, 1],
+ [0.01, 0.0, 64, 0.0001, 1024, 256, 0.85, 1e-07, 1],
+ [0.01, 0.0, 64, 0.0001, 1024, 256, 0.85, 1e-08, 1],
+ [0.01, 0.0, 64, 0.0001, 1024, 256, 1, 1e-07, 1],
+ [0.01, 0.0, 64, 0.0001, 1024, 256, 1, 1e-08, 1],
+ [0.001, 0.001, 64, 0.0001, 1024, 256, 0.85, 1e-07, 1],
+ [0.001, 0.001, 64, 0.0001, 1024, 256, 0.85, 1e-08, 1],
+ [0.001, 0.001, 64, 0.0001, 1024, 256, 1, 1e-07, 1],
+ [0.001, 0.001, 64, 0.0001, 1024, 256, 1, 1e-08, 1],
+ [0.01, 0.0, 128, 0.0001, 512, 256, 0.85, 1e-07, 1],
+ [0.01, 0.0, 128, 0.0001, 512, 256, 0.85, 1e-08, 1],
+ [0.01, 0.0, 128, 0.0001, 512, 256, 1, 1e-07, 1],
+ [0.01, 0.0, 128, 0.0001, 512, 256, 1, 1e-08, 1],
+ [0.01, 1e-05, 128, 0.0001, 512, 256, 0.85, 1e-07, 1],
+ [0.01, 1e-05, 128, 0.0001, 512, 256, 0.85, 1e-08, 1],
+ [0.01, 1e-05, 128, 0.0001, 512, 256, 1, 1e-07, 1],
+ [0.01, 1e-05, 128, 0.0001, 512, 256, 1, 1e-08, 1],
+ [0.01, 0.0001, 128, 0.0001, 512, 256, 0.85, 1e-07, 1],
+ [0.01, 0.0001, 128, 0.0001, 512, 256, 0.85, 1e-08, 1],
+ [0.01, 0.0001, 128, 0.0001, 512, 256, 1, 1e-07, 1],
+ [0.01, 0.0001, 128, 0.0001, 512, 256, 1, 1e-08, 1],
+ [0.01, 0.0, 128, 0.0001, 1024, 256, 0.85, 1e-07, 1],
+ [0.01, 0.0, 128, 0.0001, 1024, 256, 0.85, 1e-08, 1],
+ [0.01, 0.0, 128, 0.0001, 1024, 256, 1, 1e-07, 1],
+ [0.01, 0.0, 128, 0.0001, 1024, 256, 1, 1e-08, 1],
+ [0.01, 1e-05, 128, 0.0001, 1024, 256, 0.85, 1e-07, 1],
+ [0.01, 1e-05, 128, 0.0001, 1024, 256, 0.85, 1e-08, 1],
+ [0.01, 1e-05, 128, 0.0001, 1024, 256, 1, 1e-07, 1],
+ [0.01, 1e-05, 128, 0.0001, 1024, 256, 1, 1e-08, 1],
+ [0.01, 0.0001, 128, 0.0001, 1024, 256, 0.85, 1e-07, 1],
+ [0.01, 0.0001, 128, 0.0001, 1024, 256, 0.85, 1e-08, 1],
+ [0.01, 0.0001, 128, 0.0001, 1024, 256, 1, 1e-07, 1],
+ [0.01, 0.0001, 128, 0.0001, 1024, 256, 1, 1e-08, 1]]
+    
+which_param = args.runnum
+param_list = possibleparams[which_param]
+
+ratiorange = 0.8
+
+rob_range,l0_range = [1e-5,1e-4,1e-3,1e-2,1e-1],[1e-4,1e-5,1e-6]
+combos = [(i,j) for i in rob_range for j in l0_range]
+    
+print(args)
+if args.model == "ff":
+    from NN_model import Model
+    #from L2NN_model import Model
+elif args.model == "cnn":
+    from CNN_model import Model
+    assert(tf.keras.backend.image_data_format() == "channels_last")
+
+args.batch_range = [param_list[2]]
+args.lr = param_list[3]
+args.l2 = param_list[1]
+args.dropout = param_list[-3]
+args.l1_size = param_list[-5]
+args.l2_size = param_list[-4]
+
+args.robust = param_list[0]
+args.stable = 1
+args.l0 = param_list[-2]
+
+args.ratio_range = [ratiorange]
+args.data_set=args.data_set
+args.MC=False
+
+
+
+
+# Setting up training parameters
+seed = config['random_seed']
+tf.set_random_seed(seed)
+max_num_training_steps = config['max_num_training_steps']
+num_output_steps = config['num_output_steps']
+num_summary_steps = config['num_summary_steps']
+num_checkpoint_steps = config['num_checkpoint_steps']
+testing_size = config['testing_size']
+data_set = args.data_set
+num_subsets = args.num_subsets
+#initial_learning_rate = config['initial_learning_rate']
+eta = config['constant_learning_rate']
+learning_rate = tf.train.exponential_decay(args.lr,
+ 0, 5, 0.85, staircase=True)
+theta = args.stable and (not args.MC)
+num_channels, pixels_x, pixels_y = 0, 0, 0
+reshape = True
+global_step = tf.Variable(1, name="global_step")
+min_num_training_steps = int(0.4*max_num_training_steps)
+if args.robust == 0 and not args.stable and args.l0 == 0:
+  min_num_training_steps = 0
+
+
+for batch_size, subset_ratio in itertools.product(args.batch_range, args.ratio_range): #Parameters chosen with validation
+  print(batch_size, subset_ratio, args.dropout)
+
+
+  #Setting up the data and the model
+  if args.model == "ff":
+      data = input_data.load_data_set(training_size = args.train_size, validation_size=args.val_size, data_set=data_set, reshape=reshape, seed=seed)
+      num_features = data.train.images.shape[1]
+      num_classes = np.unique(data.train.labels).shape[0]
+      model = Model(num_classes, num_subsets, batch_size, args.l1_size, args.l2_size, subset_ratio, num_features, args.dropout, args.l2, args.l0, args.robust, args.reg_stability)
+      var_list = [model.W1, model.b1, model.W2, model.b2, model.W3, model.b3]
+      if args.l0 > 0:
+          var_list = [model.W1, model.log_a_W1, model.b1, model.log_a_W2, model.W2, model.b2, model.W3, model.log_a_W3, model.b3]
+    
+  elif args.model == "cnn":
+      reshape = False
+      data = input_data.load_data_set(training_size = args.train_size, validation_size=args.val_size, data_set=data_set, reshape=reshape, seed=seed)
+      print(data.train.images.shape)
+      num_classes = np.unique(data.train.labels).shape[0]
+      num_features = data.train.images.shape[1]
+      pixels_x = data.train.images.shape[1]
+      pixels_y = data.train.images.shape[2]
+      num_channels = data.train.images.shape[3]
+
+      model = Model(num_subsets, batch_size, args.cnn_size, args.fc_size, subset_ratio, pixels_x, pixels_y, num_channels, args.dropout, args.l2, theta, args.robust)
+
+
+  #Returns the right loss depending on MC or dual or nothing
+  loss = utils_model.get_loss(model, args)
+
+
+  # Setting up the optimizer
+  if (args.stable and not args.MC) or (args.model == "cnn"):
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss + model.regularizer, global_step=global_step)
+  else:
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss + model.regularizer, global_step=global_step, var_list=var_list)
+
+  
+  #Initializing loop variables.
+  avg_test_acc = 0
+  num_experiments = config['num_experiments']
+  dict_exp = utils_model.create_dict(args, num_classes, data.train.images.shape, data.test.images.shape)
+  output_dir = 'outputs/logs/' + str(args.data_set) + '/' + str(datetime.now())
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+
+  for experiment in range(num_experiments):
+
+    seed_i = seed*(experiment+1)
+    data = input_data.load_data_set(training_size = args.train_size, validation_size=args.val_size, data_set=data_set, reshape=reshape, seed=seed_i)
+
+    #Setting up data for testing and validation
+    val_dict = {model.x_input: data.validation.images,
+                  model.y_input: data.validation.labels.reshape(-1)}
+    test_dict = {model.x_input: data.test.images[:testing_size],
+                  model.y_input: data.test.labels[:testing_size].reshape(-1)}
+
+    directory = output_dir + '/exp_' + str(experiment) + '_l2reg_' + str(args.l2)
+    summary_writer = tf.summary.FileWriter(directory)
+    saver = tf.train.Saver(max_to_keep=3)
+
+    with tf.Session() as sess:
+          sess.run(tf.global_variables_initializer())
+          training_time = 0.0
+    
+          # Main training loop
+          best_val_acc, test_acc, num_iters = 0, 0, 0
+
+          for ii in range(max_num_training_steps):
+            x_batch, y_batch = data.train.next_batch(batch_size)
+            if args.model == "cnn":
+                y_batch = y_batch.reshape(-1)
+            nat_dict = {model.x_input: x_batch,
+                        model.y_input: y_batch}
+
+    
+            # Output
+            if ii % num_output_steps == 0:
+
+              val_acc = utils_print.print_metrics(sess, model, nat_dict, val_dict, test_dict, ii, args, summary_writer, global_step)
+              saver.save(sess, directory+ '/checkpoints/checkpoint', global_step=global_step)
+              
+               #Validation
+              if val_acc > best_val_acc and ii > min_num_training_steps:
+                print("New best val acc is", val_acc)
+                best_val_acc = val_acc
+                num_iters = ii
+                test_acc = sess.run(model.accuracy, feed_dict=test_dict)
+
+                print("New best test acc is", test_acc)
+                dict_exp = utils_model.update_dict(dict_exp, args, sess, model, test_dict, experiment)
+
+              if ii != 0:
+                print('    {} examples per second'.format(
+                    num_output_steps * batch_size / training_time))
+                training_time = 0.0
+
+            # Actual training step
+            start = timer()
+            sess.run(optimizer, feed_dict=nat_dict)
+            end = timer()
+            training_time += end - start
+    
+    
+          #Output test results
+          utils_print.update_dict_output(dict_exp, experiment, sess, test_acc, model, test_dict, num_iters)
+          avg_test_acc += test_acc
+          x_test, y_test = data.test.images[:testing_size], data.test.labels[:testing_size].reshape(-1)
+          best_model = utils_model.get_best_model(dict_exp, experiment, args, num_classes, num_subsets, batch_size, subset_ratio, 
+            num_features, theta, num_channels, pixels_x, pixels_y)
+          utils_print.update_adv_acc(args, best_model, x_test, y_test, experiment, dict_exp)
+
+  utils_print.print_stability_measures(dict_exp, args, num_experiments, batch_size, subset_ratio, avg_test_acc, max_num_training_steps)
---
+++ CIFAR_june29/utils_print.py
@@ -0,0 +1,208 @@
+from datetime import datetime
+import numpy as np
+import csv
+from utils import total_gini
+import tensorflow.compat.v1 as tf
+import json
+from pgd_attack import LinfPGDAttack
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+def print_metrics(sess, model, nat_dict, val_dict, test_dict, ii, args, summary_writer, global_step):
+    nat_acc = sess.run(model.accuracy, feed_dict=nat_dict)
+    val_acc = sess.run(model.accuracy, feed_dict=val_dict)
+    nat_xent = sess.run(model.xent, feed_dict=nat_dict)
+    dual_xent = sess.run(model.dual_xent, feed_dict=nat_dict)
+    MC_xent = sess.run(model.MC_xent, feed_dict=nat_dict)
+    robust_xent = sess.run(model.robust_xent, feed_dict=nat_dict)
+    robust_stable_xent = sess.run(model.robust_stable_xent, feed_dict=nat_dict)
+    theta = sess.run(model.theta, feed_dict=nat_dict)
+
+
+    print('Step {}:    ({})'.format(ii, datetime.now()))
+    print('    training nat accuracy {:.4}'.format(nat_acc * 100))
+    print('    validation nat accuracy {:.4}'.format(val_acc * 100))
+    print('    Nat Xent {:.4}'.format(nat_xent))
+
+    if args.stable:
+        print('    theta {:.4}'.format(theta))
+        print('    Stable xent upper bound with dual {:.4}'.format(dual_xent))
+        print('    Stable Xent lower bound with Monte Carlo {:.4}'.format(MC_xent))
+
+    if args.robust > 0 :
+        print('    Robust Xent {:.4}'.format(robust_xent))
+        if args.stable:
+            print('    Robust Stable Xent {:.4}'.format(robust_stable_xent))
+
+    if args.model == "ff":
+        if args.l0 > 0:
+            W1_killed_neurons = sum(np.sum(sess.run(model.W1_masked), axis=0) == 0)
+            W2_killed_neurons = sum(np.sum(sess.run(model.W2_masked), axis=0) == 0)
+            W3_killed_neurons = sum(np.sum(sess.run(model.W3_masked), axis=0) == 0)
+            W1_killed_inputs = sum(np.sum(sess.run(model.W1_masked), axis= 1) == 0)
+            W2_killed_inputs = sum(np.sum(sess.run(model.W2_masked), axis=1) == 0)
+            W3_killed_inputs = sum(np.sum(sess.run(model.W3_masked), axis=1) == 0)
+            Non_zero_W1 = sum(sess.run(model.W1_masked).reshape(-1) > 0)/sess.run(model.W1_masked).reshape(-1).shape[0]
+            Non_zero_W2 = sum(sess.run(model.W2_masked).reshape(-1) > 0)/sess.run(model.W2_masked).reshape(-1).shape[0]
+            Non_zero_W3 = sum(sess.run(model.W3_masked).reshape(-1) > 0)/sess.run(model.W3_masked).reshape(-1).shape[0]
+            print('    Killed neurons W1', W1_killed_neurons)
+            print('    Killed features input to W1', W1_killed_inputs) #sum(np.sum(sess.run(model.W1_masked), axis= 1) == 0))
+            print('    Killed neurons W2', W2_killed_neurons)
+            print('    Killed features input to W2', W2_killed_inputs)
+            print('    Killed neurons W3', W3_killed_neurons)
+            print('    Killed features input to W3', W3_killed_inputs)
+
+        else:
+            Non_zero_W1 = sum(sess.run(model.W1).reshape(-1) > 0) / sess.run(model.W1).reshape(-1).shape[0]
+            Non_zero_W2 = sum(sess.run(model.W2).reshape(-1) > 0) / sess.run(model.W2).reshape(-1).shape[0]
+            Non_zero_W3 = sum(sess.run(model.W3).reshape(-1) > 0) / sess.run(model.W3).reshape(-1).shape[0]
+        print('    Non zero W1 features percentage', Non_zero_W1)
+        print('    Non zero W2 features percentage', Non_zero_W2)
+        print('    Non zero W3 features percentage', Non_zero_W3)
+        #print('    Debugging: sum of W3', sum(sess.run(model.W3)))
+        #print('    Debugging: sum of log_a_W3', sum(sess.run(model.log_a_W3)))
+        regularizer = sess.run(model.regularizer, feed_dict=nat_dict)
+        print('    Regularizer', regularizer)
+
+
+    """
+
+    summary1 = tf.Summary(value=[tf.Summary.Value(tag='TrainAcc', simple_value=nat_acc), ])
+    summary2 = tf.Summary(value=[tf.Summary.Value(tag='ValAcc', simple_value=val_acc), ])
+    summary3 = tf.Summary(value=[tf.Summary.Value(tag='TrainXent', simple_value=nat_xent), ])
+    summary4 = tf.Summary(value=[tf.Summary.Value(tag='DualXent', simple_value=dual_xent), ])
+    if args.l0 > 0:
+        summary5 = tf.Summary(value=[tf.Summary.Value(tag='W1_killed_neurons', simple_value=W1_killed_neurons), ])
+        summary6 = tf.Summary(value=[tf.Summary.Value(tag='W2_killed_neurons', simple_value=W2_killed_neurons), ])
+        summary7 = tf.Summary(value=[tf.Summary.Value(tag='W3_killed_neurons', simple_value=W3_killed_neurons), ])
+        summary8 = tf.Summary(value=[tf.Summary.Value(tag='W1_killed_inputs', simple_value=W1_killed_inputs), ])
+        summary9 = tf.Summary(value=[tf.Summary.Value(tag='W2_killed_inputs', simple_value=W2_killed_inputs), ])
+        summary10 = tf.Summary(value=[tf.Summary.Value(tag='W3_killed_inputs', simple_value=W3_killed_inputs), ])
+
+        summary_writer.add_summary(summary5, global_step.eval(sess))
+        summary_writer.add_summary(summary6, global_step.eval(sess))
+        summary_writer.add_summary(summary7, global_step.eval(sess))
+        summary_writer.add_summary(summary8, global_step.eval(sess))
+        summary_writer.add_summary(summary9, global_step.eval(sess))
+        summary_writer.add_summary(summary10, global_step.eval(sess))
+
+    summary11 = tf.Summary(value=[tf.Summary.Value(tag='W1_Non_Zero', simple_value=Non_zero_W1), ])
+    summary12 = tf.Summary(value=[tf.Summary.Value(tag='W2_Non_Zero', simple_value=Non_zero_W2), ])
+    summary13 = tf.Summary(value=[tf.Summary.Value(tag='W3_Non_Zero', simple_value=Non_zero_W3), ])
+
+    summary_writer.add_summary(summary11, global_step.eval(sess))
+    summary_writer.add_summary(summary12, global_step.eval(sess))
+    summary_writer.add_summary(summary13, global_step.eval(sess))
+    #tensor = tf.constant(json.dumps(vars(args), indent=2))
+    #tensor = tf.constant(vars(args).keys())
+    #print(vars(args).keys())
+    #summary = tf.summary.text("Args", tensor)
+    summary_writer.add_summary(summary1, global_step.eval(sess))
+    summary_writer.add_summary(summary2, global_step.eval(sess))
+    summary_writer.add_summary(summary3, global_step.eval(sess))
+    summary_writer.add_summary(summary4, global_step.eval(sess))
+    # summary_writer.add_text('args', str(args), global_step.eval(sess))
+    if args.model == "ff":
+        try:
+            summary5 = sess.run(model.summary, feed_dict=test_dict)
+            summary_writer.add_summary(summary5, global_step.eval(sess))
+        except:
+            print("Error with NaN caught")
+    """
+    return val_acc
+
+def update_dict_output(dict_exp, experiment, sess, test_acc, model, test_dict, num_iters):
+    dict_exp['test_accs'][experiment] = test_acc*100
+    #dict_exp['thetas'][experiment] = sess.run(model.theta, feed_dict=test_dict)
+    dict_exp['iterations'][experiment] = num_iters
+
+    return dict_exp
+
+def update_adv_acc(args, best_model, x_test, y_test, experiment, dict_exp):
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        clip = True
+        if "uci" in args.data_set:
+            clip = False
+        for eps_test in args.robust_test:
+            attack = LinfPGDAttack(best_model, eps_test, config['k'], config['a'], 
+            config['random_start'], config['loss_func'], clip)
+            x_test_adv = attack.perturb(x_test, y_test, sess)
+            adv_dict = {best_model.x_input: x_test_adv, best_model.y_input: y_test}
+            dict_exp['adv_test_accs'][eps_test][experiment] = sess.run(best_model.accuracy, feed_dict=adv_dict)
+
+
+def print_stability_measures(dict_exp, args, num_experiments, batch_size, subset_ratio, avg_test_acc, max_num_training_steps):
+
+    avg_test_acc = avg_test_acc / num_experiments
+    print('  Average testing accuracy {:.4}'.format(avg_test_acc * 100))
+    print('  Individual accuracies: \n', dict_exp['test_accs'])
+    print('  Adv testing accuracies', dict_exp['adv_test_accs'])
+    print('  Theta values', dict_exp['thetas'])
+    std = np.array([float(k) for k in dict_exp['test_accs']]).std()
+    print('  Test Accuracy std {:.2}'.format(np.array([float(k) for k in dict_exp['test_accs']]).std()))
+    print("  Logits std", np.mean(np.mean(np.std(dict_exp['logits_acc'], axis=0), axis=0)))
+    logit_stability = np.mean(np.std(dict_exp['logits_acc'], axis=0), axis=0)
+    gini_stability = total_gini(dict_exp['preds'].transpose())
+    print("  Gini stability", gini_stability)
+
+    if args.model == "ff":
+        w1_stability, w2_stability, w3_stability = print_layer_stability_ff(dict_exp)
+        W1_non_zero = np.mean(dict_exp['W1_non_zero'])
+        W2_non_zero = np.mean(dict_exp['W2_non_zero'])
+        W3_non_zero = np.mean(dict_exp['W3_non_zero'])
+        print(' W1 non zero percentage', W1_non_zero)
+        print(' W2 non zero percentage', W2_non_zero)
+        print(' W3 non zero percentage', W3_non_zero)
+    elif args.model == "cnn":
+        conv11_stability, conv12_stability, conv21_stability, conv22_stability, conv31_stability, conv32_stability, fc1_stability, fc2_stability = print_layer_stability_cnn(dict_exp)
+
+    file = open(str('results_' + args.model + args.data_set + '.csv'), 'a+', newline='')
+    with file:
+        writer = csv.writer(file)
+        if args.model == "ff":
+            writer.writerow(
+                [args.stable, args.robust, args.robust_test, num_experiments, args.train_size, batch_size, subset_ratio, avg_test_acc, dict_exp['test_accs'], 
+                dict_exp['adv_test_accs'], std, dict_exp['thetas'], max_num_training_steps, dict_exp['iterations'], w1_stability, w2_stability,
+                w3_stability, logit_stability, gini_stability, args.l2, args.l0, W1_non_zero, W2_non_zero, W3_non_zero, args.l1_size, args.l2_size,
+                args.lr, dict_exp['W1_killed_input_features'], dict_exp['W1_killed_neurons'], dict_exp['W2_killed_input_features'], dict_exp['W2_killed_neurons'],
+                dict_exp['W3_killed_input_features'], dict_exp['W3_killed_neurons']])
+
+        elif args.model == "cnn":
+            writer.writerow(
+                [args.stable, args.robust, num_experiments, args.train_size, batch_size, subset_ratio, avg_test_acc, dict_exp['test_accs'], std,
+                 dict_exp['thetas'], max_num_training_steps, dict_exp['iterations'], conv11_stability, conv12_stability, conv21_stability,
+                 conv22_stability, conv31_stability, conv32_stability, fc1_stability, fc2_stability, logit_stability,
+                 gini_stability, args.l2, args.l0, args.robust])
+
+
+def print_layer_stability_ff(dict_exp):
+    w1_stability = np.mean(np.std(dict_exp['W1_acc'], axis=0), axis=0)
+    w2_stability = np.mean(np.std(dict_exp['W2_acc'], axis=0), axis=0)
+    w3_stability = np.mean(np.std(dict_exp['W3_acc'], axis=0), axis=0)
+    print("W1 std", w1_stability)
+    print("W2 std", w2_stability)
+    print("W3 std", w3_stability)
+    return w1_stability, w2_stability, w3_stability
+
+def print_layer_stability_cnn(dict_exp):
+    conv11_stability = np.mean(np.std(dict_exp['conv11_acc'], axis=0), axis=0)
+    conv12_stability = np.mean(np.std(dict_exp['conv12_acc'], axis=0), axis=0)
+    conv21_stability = np.mean(np.std(dict_exp['conv21_acc'], axis=0), axis=0)
+    conv22_stability = np.mean(np.std(dict_exp['conv22_acc'], axis=0), axis=0)
+    conv31_stability = np.mean(np.std(dict_exp['conv31_acc'], axis=0), axis=0)
+    conv32_stability = np.mean(np.std(dict_exp['conv31_acc'], axis=0), axis=0)
+    fc1_stability = np.mean(np.std(dict_exp['fc1_acc'], axis=0), axis=0)
+    fc2_stability = np.mean(np.std(dict_exp['fc2_acc'], axis=0), axis=0)
+
+    print("conv11 std", conv11_stability)
+    print("conv12 std", conv12_stability)
+    print("conv21 std", conv21_stability)
+    print("conv22 std", conv22_stability)
+    print("conv31 std", conv31_stability)
+    print("conv32 std", conv32_stability)
+    print("fc1 std", fc1_stability)
+    print("fc2 std", fc2_stability)
+
+    return conv11_stability, conv12_stability, conv21_stability, conv22_stability,conv31_stability,conv32_stability, fc1_stability, fc2_stability
