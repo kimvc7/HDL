afad3f669acb11aa13f3130fa6b9471975db4415
kimvc7 <30934898+kimvc7@users.noreply.github.com>
2022-08-20 11:38:06 -0400

    Update run_cifar_michael2.sh

--- src/run_cifar_michael2.sh
+++ src/run_cifar_michael2.sh
@@ -9,9 +9,9 @@
 #SBATCH --time=1-23:30:00
 #SBATCH --mem-per-cpu=4G
 
-#SBATCH --array=200-359
+#SBATCH --array=200-399
 
 source /etc/profile ;
 module load anaconda/2021a
 
-python train.py --train_size 0.1 --data_set cifar10 --network_type ALEX --exp_id ${SLURM_ARRAY_TASK_ID}
+python train.py --train_size 1 --data_set mnist --network_type ALEX --exp_id ${SLURM_ARRAY_TASK_ID}
