1f1c7f77f41dc7e86296cf8267909db06fcb9385
kimvc7 <30934898+kimvc7@users.noreply.github.com>
2022-09-12 14:38:40 -0400

    Update input_data.py

--- src/input_data.py
+++ src/input_data.py
@@ -175,6 +175,7 @@ def load_data_set(training_size, validation_size, data_set, seed=None, reshape=T
     (X_train, y_train), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
     num_features = X_train.shape[1]*X_train.shape[2]
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=validation_size, random_state=seed)
+    
   elif data_set == "mnist":
     (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
     if not reshape:
