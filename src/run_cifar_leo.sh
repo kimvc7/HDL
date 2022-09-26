ef76836248e290241cdefcc7595bdb9202de86ef
kimvc7 <30934898+kimvc7@users.noreply.github.com>
2022-09-26 14:24:17 -0400

    Update run_cifar_leo.sh

--- src/run_cifar_leo.sh
+++ src/run_cifar_leo.sh
@@ -14,4 +14,4 @@
 source /etc/profile ;
 module load anaconda/2021a
 
-python train.py --train_size 0.6 --data_set cifar10 --network_type ALEX --exp_id ${SLURM_ARRAY_TASK_ID}
+python train.py --train_size 1 --data_set cifar10 --network_type MLP --exp_id ${SLURM_ARRAY_TASK_ID}
